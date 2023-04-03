c79985768e3b8db019887639de9e87da82482fa8
terasfly <84103159+terasfly@users.noreply.github.com>
2023-04-03 22:37:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const api = 'aa2ed12d93ba18e5ef77cfe8606163d5'
 
 const weatherClass = {
     Clouds: 'images/cloud.svg',
-    Rain: 'images/rain.svg',
+    broken: 'images/broken.svg',
     Clear: 'images/sun.svg',
     Snow: 'images/snow.svg',
-    lightRain: 'images/light.svg'
+    few: 'images/light.svg',
+    clear: 'images/sun.svg',
+    light: 'images/lightSnow.svg',
+    overcast: 'images/cloud.svg'
+
 }
 
 
@@ -35,6 +39,29 @@ btn.addEventListener('click', () => {
                 const temp = data.list[0].main.temp
                 const roundTempNow = Math.round(temp)
                 document.querySelector('.weather__temp').textContent = roundTempNow;
+
+
+                // wheatherCondition NOW
+
+
+                const wheatherConditionNow = data.list[0].weather[0].description
+                const description1 = wheatherConditionNow.split(' ')[0]
+
+                console.log(description1)
+
+
+                const wheatherIcon = document.createElement('img')
+                wheatherIcon.src = weatherClass[description1]
+                wheatherIcon.style.width = '100px'
+                wheatherIcon.style.height = '100px'
+
+                const icon = document.getElementById('icon')
+                icon.innerHTML = '';
+                // console.log(wheatherIcon)
+                icon.appendChild(wheatherIcon)
+
+                // weather 15 o'clock
+
                 //   1st time
                 const time = data.list[0].dt_txt
                 console.log(time)
@@ -62,21 +89,6 @@ btn.addEventListener('click', () => {
                 document.getElementById('four__time').textContent = timeOnly4
 
 
-                const wheatherConditionNow = data.list[0].weather[0].main;
-                const wheatherIcon = document.createElement('img')
-                wheatherIcon.src = weatherClass[wheatherConditionNow]
-                wheatherIcon.style.width = '100px'
-                wheatherIcon.style.height = '100px'
-
-                const icon = document.getElementById('icon')
-                icon.innerHTML = '';
-                // console.log(wheatherIcon)
-                icon.appendChild(wheatherIcon)
-
-                // weather 15 o'clock
-
-
-
 
             })
             .catch((error) => console.log(error));
@@ -84,11 +96,13 @@ btn.addEventListener('click', () => {
     })
     // 12:00 o'clock
 function twelveTime(data) {
-    const tempTwelve = data.list[2].main.temp;
+    const tempTwelve = data.list[0].main.temp;
     const roundTempTwelve = Math.round(tempTwelve)
     document.querySelector('.twelve').textContent = roundTempTwelve;
 
-    const airConditionTvelweTime = data.list[2].weather[0].main;
+    const airConditionTvelweTime = data.list[0].weather[0].main;
+    // const airgfg = airConditionTvelwe.split(' ')[0]
+    // console.log(airgfg)
 
 
     const airConditionTvelwe = document.createElement('img')
@@ -103,11 +117,11 @@ function twelveTime(data) {
 }
 // 15:00 o'clock
 function threeTime(data) {
-    const tempThree = data.list[6].main.temp;
+    const tempThree = data.list[1].main.temp;
     const roundTempThree = Math.round(tempThree)
     document.querySelector('.three').textContent = roundTempThree;
 
-    const airConditionThreeTime = data.list[6].weather[0].main;
+    const airConditionThreeTime = data.list[1].weather[0].main;
     const airConditionThree = document.createElement('img')
     airConditionThree.src = weatherClass[airConditionThreeTime]
     airConditionThree.style.width = '50px'
@@ -119,13 +133,14 @@ function threeTime(data) {
 }
 
 function sixTime(data) {
-    const tempSix = data.list[11].main.temp;
+    const tempSix = data.list[2].main.temp;
     const roundTempSix = Math.round(tempSix)
     document.querySelector('.six').textContent = roundTempSix;
 
-    const airConditionSixTime = data.list[11].weather[0].main;
+    const airConditionSixTime = data.list[2].weather[0].description;
+    const description3 = airConditionSixTime.split(' ')[0]
     const airConditionSix = document.createElement('img')
-    airConditionSix.src = weatherClass[airConditionSixTime]
+    airConditionSix.src = weatherClass[description3]
     airConditionSix.style.width = '50px'
     airConditionSix.style.height = '50px'
     const airConditionSixSky = document.getElementById('airConditionSixSky')
@@ -135,11 +150,11 @@ function sixTime(data) {
 }
 
 function nineTime(data) {
-    const tempNine = data.list[12].main.temp;
+    const tempNine = data.list[3].main.temp;
     const roundTempNine = Math.round(tempNine)
     document.querySelector('.nine').textContent = roundTempNine;
 
-    const airConditionNineTime = data.list[12].weather[0].main;
+    const airConditionNineTime = data.list[3].weather[0].main;
     const airConditionNine = document.createElement('img')
     airConditionNine.src = weatherClass[airConditionNineTime]
     airConditionNine.style.width = '50px'
