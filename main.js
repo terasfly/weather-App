59ef5afe83d70bf8517ea6e97be7fe53d96b96bf
terasfly <84103159+terasfly@users.noreply.github.com>
2023-04-21 00:53:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ const btn = document.getElementById('btn');
 const api = 'aa2ed12d93ba18e5ef77cfe8606163d5'
 const defaultCity = 'Belfast'
 
-const container = document.querySelector('.forecast__grid');
-let startY, currentY, previousY = 0;
-let isDragging = false;
-let currentIndex = 0;
+
 
 
 
@@ -19,7 +16,9 @@ const weatherClass = {
     light: 'images/lightRain.svg',
     overcast: 'images/cloud.svg',
     scattered: 'images/broken.svg',
-    moderate: 'images/havy.svg'
+    moderate: 'images/havy.svg',
+    night: 'images/clearNight.svg',
+    nightClouds: 'images/fewCloudsNight.svg'
 
 }
 
@@ -225,14 +224,40 @@ function btnChangeTemp(data) {
 
 // 12:00 o'clock
 function twelveTime(data) {
+    const time = data.list[0].dt_txt
+    console.log(time)
+
+    const dataSplit = time.split(' ')[1]
+
+    const timeOnly = dataSplit.substring(0, 5)
+        // console.log(timeOnly)
+        // document.getElementById('first__time').textContent = timeOnly
+
+    const makeNumber = parseInt(timeOnly)
+    console.log(makeNumber)
+
     const tempTwelve = data.list[0].main.temp;
     const roundTempTwelve = Math.round(tempTwelve)
     document.querySelector('.twelve').textContent = roundTempTwelve;
 
     const airConditionTvelweTime = data.list[0].weather[0].description;
-    const description1 = airConditionTvelweTime.split(' ')[0]
+    let description1 = airConditionTvelweTime.split(' ')[0];
+    console.log(description1)
+    if (makeNumber > 21 || makeNumber < 6) {
 
-
+        if (description1 === 'clear') {
+            description1 = 'night'
+        } else if (description1 === 'few') {
+            description1 = 'nightClouds'
+        } else if (description1 === 'broken') {
+            description1 = 'nightClouds'
+        } else if (description1 === 'scattered') {
+            description1 = 'nightClouds'
+        }
+    }
+    // if (hour >= 1 || hour <= 2) {
+    //     description1 = 'Snow'; // set the weather condition to Snow if it's after 21:00
+    // }
     const airConditionTvelwe = document.createElement('img')
     airConditionTvelwe.src = weatherClass[description1]
     airConditionTvelwe.style.width = '50px'
@@ -242,15 +267,40 @@ function twelveTime(data) {
     airConditionTvelweSky.innerHTML = '';
     airConditionTvelweSky.appendChild(airConditionTvelwe)
 
+
 }
 // 15:00 o'clock
 function threeTime(data) {
+    const time = data.list[1].dt_txt
+    console.log(time)
+
+    const dataSplit = time.split(' ')[1]
+
+    const timeOnly = dataSplit.substring(0, 5)
+    console.log(timeOnly)
+        // document.getElementById('first__time').textContent = timeOnly
+
+    const makeNumber = parseInt(timeOnly)
+    console.log(makeNumber)
+
     const tempThree = data.list[1].main.temp;
     const roundTempThree = Math.round(tempThree)
     document.querySelector('.three').textContent = roundTempThree;
 
     const airConditionThreeTime = data.list[1].weather[0].description;
-    const description2 = airConditionThreeTime.split(' ')[0]
+    let description2 = airConditionThreeTime.split(' ')[0]
+    if (makeNumber > 21 || makeNumber < 6) {
+
+        if (description2 === 'clear') {
+            description2 = 'night'
+        } else if (description2 === 'few') {
+            description2 = 'nightClouds'
+        } else if (description2 === 'broken') {
+            description2 = 'nightClouds'
+        } else if (description2 === 'scattered') {
+            description2 = 'nightClouds'
+        }
+    }
     const airConditionThree = document.createElement('img')
     airConditionThree.src = weatherClass[description2]
     airConditionThree.style.width = '50px'
@@ -262,12 +312,36 @@ function threeTime(data) {
 }
 
 function sixTime(data) {
+    const time = data.list[2].dt_txt
+    console.log(time)
+
+    const dataSplit = time.split(' ')[1]
+
+    const timeOnly = dataSplit.substring(0, 5)
+    console.log(timeOnly)
+        // document.getElementById('first__time').textContent = timeOnly
+
+    const makeNumber = parseInt(timeOnly)
+    console.log(makeNumber)
+
     const tempSix = data.list[2].main.temp;
     const roundTempSix = Math.round(tempSix)
     document.querySelector('.six').textContent = roundTempSix;
 
     const airConditionSixTime = data.list[2].weather[0].description;
-    const description3 = airConditionSixTime.split(' ')[0]
+    let description3 = airConditionSixTime.split(' ')[0]
+    if (makeNumber > 21 || makeNumber < 6) {
+
+        if (description3 === 'clear') {
+            description3 = 'night'
+        } else if (description3 === 'few') {
+            description3 = 'nightClouds'
+        } else if (description3 === 'broken') {
+            description3 = 'nightClouds'
+        } else if (description3 === 'scattered') {
+            description3 = 'nightClouds'
+        }
+    }
     console.log(description3)
     const airConditionSix = document.createElement('img')
     airConditionSix.src = weatherClass[description3]
@@ -280,12 +354,36 @@ function sixTime(data) {
 }
 
 function nineTime(data) {
+    const time = data.list[2].dt_txt
+    console.log(time)
+
+    const dataSplit = time.split(' ')[1]
+
+    const timeOnly = dataSplit.substring(0, 5)
+    console.log(timeOnly)
+        // document.getElementById('first__time').textContent = timeOnly
+
+    const makeNumber = parseInt(timeOnly)
+    console.log(makeNumber)
+
     const tempNine = data.list[3].main.temp;
     const roundTempNine = Math.round(tempNine)
     document.querySelector('.nine').textContent = roundTempNine;
 
     const airConditionNineTime = data.list[3].weather[0].description;
-    const description4 = airConditionNineTime.split(' ')[0]
+    let description4 = airConditionNineTime.split(' ')[0]
+    if (makeNumber > 21 || makeNumber < 6) {
+
+        if (description4 === 'clear') {
+            description4 = 'night'
+        } else if (description4 === 'few') {
+            description4 = 'nightClouds'
+        } else if (description4 === 'broken') {
+            description4 = 'nightClouds'
+        } else if (description4 === 'scattered') {
+            description4 = 'nightClouds'
+        }
+    }
     const airConditionNine = document.createElement('img')
     airConditionNine.src = weatherClass[description4]
     airConditionNine.style.width = '50px'
